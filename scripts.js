a4c3f28c3b9ba35bd97c1bb4b58d11be65758574
nicolasdosanjosd <152753537+nicolasdosanjosd@users.noreply.github.com>
2024-09-19 20:29:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,3 +40,21 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
     "max-glare":
      0.5
 });
+
+// preload
+gsap.fromTo(".loading-page", {opacity:1}, {
+    opacity:0,
+    duration:1.5,
+    delay:3.5,
+    display: "none"
+});
+
+gsap.fromTo(".logo-name" , {
+    y:50,
+    opacity:0,
+}, {
+    y:0,
+    opacity:1,
+    duration:2,
+    delay:0.5,
+})
